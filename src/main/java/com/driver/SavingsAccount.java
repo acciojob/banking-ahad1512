1defa3065cbe781013f2c1bfc041296acccd9f0e
ahad1512 <ahadk747@gmail.com>
2023-08-30 04:41:28 +0530

    A1

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -1,28 +1,53 @@
 package com.driver;
 
 public class SavingsAccount extends BankAccount{
-    double rate;
-    double maxWithdrawalLimit;
+   private double rate;
+    private double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
-
+       super(name,balance,0);
+       this.maxWithdrawalLimit = maxWithdrawalLimit;
+       this.rate = rate;
     }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
+    public void setMaxWithdrawalLimit(double maxWithdrawalLimit) {
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+    }
+
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-
+       if (amount > maxWithdrawalLimit){
+           throw new Exception("Maximum Withdraw Limit Exceed");
+       }
+       if(amount > getBalance()){
+           throw new Exception("Insufficient Balance");
+       }
+       super.withdraw(amount);
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-
+     return ((getBalance()*rate*years)/100);
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+     double total = getBalance()*Math.pow((1+((rate/100)/times)),(times*years));
+     return (total - getBalance());
     }
-
 }
